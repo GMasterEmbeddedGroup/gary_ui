20af59b82d09b88aac38d61dde61dc3411b61e4f
Juntong <juntong_2021@outlook.com>
2023-05-02 11:26:06 +0800

    wip: 拉屎

--- gary_ui/__init__.py
+++ gary_ui/__init__.py
@@ -4,6 +4,8 @@
 from encoder import (Line, Sentence, Cycle, Rectangle, Float,
                      encode_line, encode_sentence, encode_cycle, encode_rectangle, encode_float)
 
+from loader import Loader
+
 import encoder
 import loader
 
---
+++ gary_ui/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Docstring for the main.py module.
+
+@author juntong
+@date   2023-05-02
+
+主文件
+
+"""
+
+from . import Loader
+
