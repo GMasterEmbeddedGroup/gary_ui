07b636ea5dfa8f0d77956f7cdf6b4e3e7d2397d4
juntong <juntong_2021@outlook.com>
2023-06-08 00:31:29 +0800

    feat: copy main.py back

--- gary_ui/__init__.py
+++ gary_ui/__init__.py
@@ -6,8 +6,9 @@ from .encoder import (Line, Sentence, Cycle, Rectangle, Float,
                       encode_line, encode_sentence, encode_cycle, encode_rectangle, encode_float)
 
 from .loader import Loader
+from .main import run_ui_node
 
 from . import encoder
 from . import loader
 
-__all__ = ["encoder", "loader"]
+__all__ = ["encoder", "loader", "run_ui_node"]
